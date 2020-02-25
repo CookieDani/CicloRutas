2afb2adb7a82e373eba3e44b70c7a2e1cc26080d
Daniel Peralta <soyyo.peralta@gmail.com>
2020-02-24 19:19:33 -0500

    Se agrego login

---
+++ manejadorestudiantes/src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col s12 m8 offset-m2">
+                    <div class="login card-panel black white-text center">
+                        <h3>Login</h3>
+                        <form>
+                            <div class="input-field">
+                                <i class="material-icons prefix">email</i>
+                                <input type="text" id='email'>
+                                <label for="email">Correo</label>
+                            </div>
+                            <div class="input-field">
+                                <i class="material-icons prefix">lock</i>
+                                <input type="password" id="password">
+                                <label for="password">Contraseña</label>
+                            </div>
+                            <button class="btn btn-large blue lighten-4 black-text">Ingreso</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
